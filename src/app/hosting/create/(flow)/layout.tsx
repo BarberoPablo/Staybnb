9b8db2df6d108391e674b1b718688e79f26e6921
Pablo Barbero <pablobarbero220@gmail.com>
2025-07-04 19:51:48 -0300

    fix: basic layout

--- src/app/hosting/create/(flow)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function CreateListingLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col justify-between">
-      <main className="flex-grow">{children}</main>
-    </div>
-  );
-}
--- src/app/hosting/create/page.tsx
+++ src/app/hosting/create/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 // To avoid having "/hosting/create" as an empty URL
 export default function CreateRedirectPage() {
-  redirect("/hosting/create/type");
+  redirect("/hosting/create/structure");
 }
