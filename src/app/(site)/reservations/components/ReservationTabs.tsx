81e0771c0a09fd80c643b8a3c53ecd885baf35d8
Pablo Barbero <pablobarbero220@gmail.com>
2025-07-22 00:51:09 -0300

    feat: showing user reservations with rsuite Tabs

---
+++ src/app/(site)/reservations/components/ReservationTabs.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useUser } from "@/hooks/useUser";
+import { api } from "@/lib/api/api";
+import { ResumedReservationWithListing } from "@/lib/types/reservation";
+import { useEffect, useState } from "react";
+import { Tabs } from "rsuite";
+import "rsuite/Tabs/styles/index.css";
+import ListingReservation from "./ListingReservation";
+
+type userReservations = {
+  total: number;
+  active: ResumedReservationWithListing[];
+  canceled: ResumedReservationWithListing[];
+  old: ResumedReservationWithListing[];
+};
+
+export function ReservationTabs() {
+  const { user, loading } = useUser();
+  const [userReservations, setUserReservations] = useState<userReservations>({
+    total: 0,
+    active: [],
+    canceled: [],
+    old: [],
+  });
+  const [error, setError] = useState("");
+  const [loadingReservations, setLoadingReservations] = useState(false);
+  const tabs = [
+    { eventKey: "active", title: "Active", reservations: userReservations.active },
+    { eventKey: "old", title: "Old", reservations: userReservations.old },
+    { eventKey: "canceled", title: "Canceled", reservations: userReservations.canceled },
+  ];
+
+  useEffect(() => {
+    if (!loading && user) {
+      setLoadingReservations(true);
+      const fetchReservations = async () => {
+        try {
+          const reservations = await api.getUserReservations();
+          const now = new Date();
+          setUserReservations({
+            total: reservations.length,
+            active: reservations.filter((r) => r.status === "active" && new Date(r.endDate) >= now),
+            canceled: reservations.filter((r) => r.status === "canceled"),
+            old: reservations.filter((r) => r.status === "active" && new Date(r.endDate) < now),
+          });
+        } catch (error) {
+          if (error instanceof Error) {
+            setError(error.message);
+          } else {
+            setError("Something went wrong");
+          }
+        } finally {
+          setLoadingReservations(false);
+        }
+      };
+      fetchReservations();
+    }
+  }, [user, loading]);
+
+  if (loading || loadingReservations) {
+    return <div>Loading reservations...</div>;
+  }
+
+  if (!loading && !loadingReservations && userReservations.total === 0) {
+    return <div>No reservations found</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        <p className="text-red-600 font-semibold my-4">{error}</p>
+      </div>
+    );
+  }
+
+  return (
+    <Tabs defaultActiveKey="active" appearance="tabs">
+      {tabs.map((tab) => (
+        <Tabs.Tab key={tab.eventKey} eventKey={tab.eventKey} title={tab.title} disabled={tab.reservations.length === 0}>
+          <ReservationTab title={tab.title} reservations={tab.reservations} />
+        </Tabs.Tab>
+      ))}
+    </Tabs>
+  );
+}
+
+function ReservationTab({ reservations }: { title: string; reservations: ResumedReservationWithListing[] }) {
+  return (
+    <div className="flex flex-col border-x border-b border-gray-300 rounded-tr-lg rounded-b-lg">
+      {reservations.map((reservation, index) => (
+        <div key={`${reservation.listingId}-${reservation.createdAt}`}>
+          <ListingReservation reservation={reservation} key={`${reservation.listingId}-${reservation.createdAt}`} />
+          {index !== reservations.length - 1 && <hr className="text-gray-300 mt-2" />}
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/(site)/reservations/page.tsx
+++ src/app/(site)/reservations/page.tsx
@@ -1,56 +1,17 @@
 "use client";
 
-import { useUser } from "@/hooks/useUser";
-import { api } from "@/lib/api/api";
-import { ResumedReservationWithListing } from "@/lib/types/reservation";
-import { useEffect, useState } from "react";
-import ListingReservation from "./components/ListingReservation";
+import "rsuite/Tabs/styles/index.css";
+import { ReservationTabs } from "./components/ReservationTabs";
 
 export default function ReservationsPage() {
-  const { user, loading } = useUser();
-  const [userReservations, setUserReservations] = useState<ResumedReservationWithListing[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    if (!loading && user) {
-      const fetchReservations = async () => {
-        try {
-          const reservations = await api.getUserReservations();
-
-          setUserReservations(reservations);
-        } catch (error) {
-          if (error instanceof Error) {
-            setError(error.message);
-          } else {
-            setError("Something went wrong");
-          }
-        }
-      };
-      fetchReservations();
-    }
-  }, [user, loading]);
-
-  if (loading) {
-    return <div>Loading reservations...</div>;
-  }
-
-  if (!loading && !userReservations) {
-    return <div>No reservations found</div>;
-  }
-
   return (
-    <div>
-      <h1>Reservations</h1>
+    <div className="flex flex-col gap-8">
+      <h1 className="font-semibold text-3xl">My Reservations</h1>
 
-      <div className="border border-gray-300 rounded-2xl">
-        {error && <p className="text-red-600 font-semibold my-4">{error}</p>}
-        {userReservations.length > 0 &&
-          userReservations.map((reservation, index) => (
-            <div key={`${reservation.listingId}-${reservation.createdAt}`}>
-              <ListingReservation reservation={reservation} />
-              {index !== userReservations.length - 1 && <hr className="text-gray-300 mt-2" />}
-            </div>
-          ))}
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-8">
+          <ReservationTabs />
+        </div>
       </div>
     </div>
   );
