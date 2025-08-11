0b0a91655fea4dccd511b2099854c98d0e435cce
Pablo Barbero <pablobarbero220@gmail.com>
2025-08-11 17:44:13 -0300

    fix: email on email redirection

--- src/app/(site)/auth/components/auth.ts
+++ src/app/(site)/auth/components/auth.ts
@@ -11,7 +11,7 @@ export async function signUp(email: string, password: string) {
     email,
     password,
     options: {
-      emailRedirectTo: "http://localhost:3000/auth/callback",
+      emailRedirectTo: `http://localhost:3000/auth/callback?email=${encodeURIComponent(email)}`,
     },
   });
 }
