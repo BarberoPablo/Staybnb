b1c6ace69620f276b040694715fa11a5c7afd325
Pablo Barbero <pablobarbero220@gmail.com>
2025-09-09 12:54:16 -0300

    feat/STAYBNB-61

--- src/app/(hosting)/hosting/listings/page.tsx
+++ src/app/(hosting)/hosting/listings/page.tsx
@@ -1,3 +1,18 @@
-export default function HostListingPage() {
-  return null;
+import HostListingCards from "./components/HostListingCards";
+import HostListingsHeader from "./components/HostListingsHeader";
+import HostListingsStatus from "./components/HostListingsStatus";
+import { getHostListings } from "@/lib/api/server/api";
+
+export default async function HostListingPage() {
+  const listings = await getHostListings();
+
+  return (
+    <div className="flex flex-col w-full flex-grow min-h-screen py-8 px-6 sm:px-12 gap-8">
+      <HostListingsHeader />
+
+      <HostListingCards listings={listings} />
+
+      <HostListingsStatus listings={listings} />
+    </div>
+  );
 }
